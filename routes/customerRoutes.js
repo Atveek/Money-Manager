42cf121c48dfba21de738f94f6a0257c43144494
Atveek_Dungarani <atveekdungarani2021@gmail.com>
2025-04-17 09:33:09 +0530

    Clean main branch

--- routes/customerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { authBuisness } = require("../middleware/authBuisness");
-const route = express.Router();
-const AddCustomer = require("../controllers/customerCtrl");
-
-route.post("/addCustomer", authBuisness, AddCustomer);
-
-module.exports = route;
