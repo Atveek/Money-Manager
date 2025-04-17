4092b791429c23d600e612e096d1d8346bf030e0
Atveek_Dungarani <atveekdungarani2021@gmail.com>
2025-04-17 11:55:04 +0530

    change in code

--- routes/supplierRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { authBuisness } = require("../middleware/authBuisness");
-const route = express.Router();
-const Addsupplier = require("../controllers/supplierCtrl");
-
-route.post("/addSupplier", authBuisness, Addsupplier);
-
-module.exports = route;
